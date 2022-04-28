10ae8889ae39fe4a68d287a288fae7447b96576d
Yusuf <uthmanyusuf1998@gmail.com>
2022-04-29 00:44:12 +0100

    5. 101

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,19 +6,13 @@
  * @n: pointer of an unsigned long int.
  * @index: index of the bit.
  *
- * Return: 1 if it worked, -1 if it didn't.
  */
-int clear_bit(unsigned long int *n, unsigned int index)
-{
-	unsigned int m;
+ unsigned int flip_bits(unsigned long int n, unsigned long int m)
+ {
+ 	int shift = (sizeof(n) * BYTE_LENGHT);
+ 	int dist = 0;
 
-	if (index > 63)
-		return (-1);
-
-	m = 1 << index;
-
-	if (*n & m)
-		*n ^= m;
-
-	return (1);
-}
+ 	while (shift--)
+ 		dist += (n >> shift & 1) != (m >> shift & 1);
+ 	return (dist);
+ }
