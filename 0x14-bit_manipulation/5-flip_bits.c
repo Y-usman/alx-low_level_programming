b4d965ff673f76af7937efdd28092379bda4b5cc
Yusuf <uthmanyusuf1998@gmail.com>
2022-04-29 00:45:36 +0100

    5. 101

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- * clear_bit - sets the value of a bit to 0.
- * at a given index.
- * @n: pointer of an unsigned long int.
- * @index: index of the bit.
+ * flip_bits - returns hamming distance of two bit words
+ * which is the number of bits you would need to flip to get
+ * from one number to another
+ * @n: the first bit word
+ * @m: the second bit word
  *
+ * Return: the hamming distance
  */
+
  unsigned int flip_bits(unsigned long int n, unsigned long int m)
  {
  	int shift = (sizeof(n) * BYTE_LENGHT);
